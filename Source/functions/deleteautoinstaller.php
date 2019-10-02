18324a3725c75d19329543d9170e18a574081565
Salvador Rueda <salvador.rueda@gmail.com>
2019-10-02 14:08:12 +0200

    SQL injection quick fix

--- Source/functions/deleteautoinstaller.php
+++ Source/functions/deleteautoinstaller.php
@@ -5,7 +5,7 @@ include_once '.././configs/isset_session.php';
 
 $id = $_GET['id'];
 
-$sql = "DELETE FROM saveautoinstaller WHERE id = $id";
+$sql = "DELETE FROM saveautoinstaller WHERE id = '$id';";
 mysqli_query($db, $sql);
 
 header('Location: .././index.php');
